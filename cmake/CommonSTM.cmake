c697d877ee58941ed04011aa792e4a3488c2f464
Robin Mueller <robin.mueller.m@gmail.com>
2021-08-09 13:11:29 +0200

    updated common stm file

--- cmake/CommonSTM.cmake
+++ cmake/CommonSTM.cmake
@@ -5,7 +5,7 @@ option(BUILD_F303VC "Build for F303VC board" OFF)
 
 if(NOT BUILD_H743ZI AND NOT BUILD_F303VC)
     message(
-        FATAL_ERROR "Please pick at least once board and supply it with -DBUILD_<BOARD>"
+        FATAL_ERROR "Please pick at least one board and supply it with -DBUILD_<BOARD>=ON"
     )
 endif()
 
@@ -14,6 +14,8 @@ set(STM32_CUBE_F3_PATH ${REPO_ROOT}/STM32CubeF3 PARENT_SCOPE)
 set(STM32_TOOLCHAIN_PATH $ENV{STM32_TOOLCHAIN_PATH} PARENT_SCOPE)
 set(TARGET_TRIPLET "arm-none-eabi" PARENT_SCOPE)
 set(FREERTOS_PATH ${REPO_ROOT}/FreeRTOS-Kernel PARENT_SCOPE)
+set(STM32_CMAKE_PATH ${REPO_ROOT}/stm32-cmake)
+set(STM32_CMAKE_PATH ${STM32_CMAKE_PATH} PARENT_SCOPE)
 
 # This must come before the project call!
 set(CMAKE_TOOLCHAIN_FILE ${STM32_CMAKE_PATH}/cmake/stm32_gcc.cmake PARENT_SCOPE)
